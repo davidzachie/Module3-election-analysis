2b8f120ec80b1888e1644bc14bd156008c43839d
David Nathanael <davidnathanael@davids-mbp.lan>
2021-09-13 08:48:43 -0700

    Adding Module3 Challenge Result

--- PyPoll_Challenge_starter_code.py
+++ PyPoll_Challenge_starter_code.py
@@ -124,8 +124,7 @@ with open(file_to_save, "w") as txt_file:
         f"\n"
         f"-------------------------\n"
         f"Largest County Turnout: {largest_county_turnout}\n"
-        f"-------------------------\n"
-        f"\n")
+        f"-------------------------\n")
     print(largest_county_result)
 
 
@@ -141,8 +140,7 @@ with open(file_to_save, "w") as txt_file:
         votes = candidate_votes.get(candidate_name)
         vote_percentage = float(votes) / float(total_votes) * 100
         candidate_results = (
-            f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n"
-            f"\n")
+            f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
         # Print each candidate's voter count and percentage to the
         # terminal.
